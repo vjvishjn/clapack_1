c04e650734ecd41ddf2b6971bdcdf55166c10b0b
Ashwani Chandil <chandil@adobe.com>
2022-01-27 17:21:04 +0530

    Fixing compilation bugs

--- F2CLIBS/libf2c/ef1asc_.c
+++ F2CLIBS/libf2c/ef1asc_.c
@@ -10,10 +10,10 @@ extern "C" {
 #define EVEN(x)	( ( (x)+ M) & (~M) )
 
 #ifdef KR_headers
-extern VOID s_copy();
+extern int s_copy();
 ef1asc_(a, la, b, lb) ftnint *a, *b; ftnlen *la, *lb;
 #else
-extern void s_copy(char*,char*,ftnlen,ftnlen);
+extern int s_copy(char*,char*,ftnlen,ftnlen);
 int ef1asc_(ftnint *a, ftnlen *la, ftnint *b, ftnlen *lb)
 #endif
 {
